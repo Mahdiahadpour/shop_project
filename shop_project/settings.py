d75170b6ab42fbec9bf1123157ee405a2e6cdbe9
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-06 02:27:53 +0430

    adding other apps
    adding other apps and adding all apps to installed apps in setting.py

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
+    'customers',
+    'product',
+    'orders'
 ]
 
 MIDDLEWARE = [
