0346b9a62c873ea9451893423ea2ba9dfccb0d57
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-06 02:31:07 +0430

    adding rest frame work versioning

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# rest frame work versioning
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning'
+}
