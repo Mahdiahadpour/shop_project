e03626963dbf08a6e54ab21cb906af5f0fd1f6d3
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-07 05:48:35 +0430

    adding some models to customer - 2

--- customers/models.py
+++ customers/models.py
@@ -5,13 +5,25 @@ from django.db import models
 # Create your models here.
 class CostumerInfo(models.Model):
     costumer_user = models.ForeignKey(User, on_delete=models.RESTRICT)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=11)
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100, null=False)
+    birthday = models.DateField()
+    ID_card_number = models.PositiveSmallIntegerField(unique=True, max_length=10)
+    E_mail = models.EmailField()
 
 
-class CostumerAddress(models.Model):
-    address_name = models.CharField(max_length=60)
+# going to be a form
+class Address(models.Model):
+    address_name = models.CharField(max_length=50)
+    providence = models.CharField(max_length=16)
+    city = models.CharField(max_length=35)
+    address_detail = models.TextField()
+
+
+class CostumerContactInfo(models.Model):
+    mobile_number = models.CharField(max_length=11, null=False, unique=True)
+    home_number = models.CharField(max_length=11)
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
 
 
 class CostumerOrderHistory(models.Model):
