eb88729879d37911b30648ab854dd04c4bf85875
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-07 06:46:25 +0430

    adding some models to product - 1

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    sub_category = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.PositiveIntegerField()
+    category = models.OneToOneField(Category, on_delete=models.PROTECT)
