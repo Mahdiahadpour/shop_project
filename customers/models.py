91c7e7640f8a159343d0556551a223830451cbb9
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-07 05:26:34 +0430

    adding some models to customer - 1

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class CostumerInfo(models.Model):
+    costumer_user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11)
+
+
+class CostumerAddress(models.Model):
+    address_name = models.CharField(max_length=60)
+
+
+class CostumerOrderHistory(models.Model):
+    pass
