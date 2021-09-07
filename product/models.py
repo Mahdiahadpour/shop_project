bd508865dd5fd6d83d5931ea910a107f3e62359b
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-07 07:02:56 +0430

    adding some models to product - 2

--- product/models.py
+++ product/models.py
@@ -11,4 +11,23 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.PositiveIntegerField()
+    price_discount = models.PositiveIntegerField()
     category = models.OneToOneField(Category, on_delete=models.PROTECT)
+    size_choices = (
+        ('extra small', 0),
+        ('small', 1),
+        ('medium', 2),
+        ('large', 3),
+        ('extra large', 4)
+    )
+    size = models.PositiveSmallIntegerField(choices=size_choices)
+    color = models.CharField(max_length=25)
+    unit_in_stock = models.PositiveIntegerField()
+    ranking_choices = (
+        ('Very Bad', 1),
+        ('Bad', 2),
+        ('Not Bad', 3),
+        ('Good', 4),
+        ('Very Good', 5),
+    )
+    ranking = models.PositiveIntegerField(choices=ranking_choices)
