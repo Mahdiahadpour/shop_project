fcb14a1a1a819ac7fb2d74185b09304cf0e98c82
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-07 07:17:29 +0430

    adding some models to Orders - 1

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from customers.models import Customer
+from product.models import Product
+
+
+class OrderDetails(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+    quantity = models.PositiveIntegerField()
+    total = models.PositiveIntegerField()
+    fulfilled = models.BooleanField(default=False)
+    fulfilled_time = models.DateField()
+    order_date = models.DateTimeField()
+    costumer_massage = models.TextField()
+
+
+class Payment(models.Model):
+    payment_type_choices = (
+        ('Online', 1),
+        ('On deliver', 2)
+    )
+    payment_type = models.PositiveSmallIntegerField(choices=None)
+    payed = models.BooleanField(default=False)
+
+
+class Orders(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
+    order_detail = models.OneToOneField(OrderDetails, on_delete=models.PROTECT)
+    deleted = models.BooleanField(default=False)
