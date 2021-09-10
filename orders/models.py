4c752c34713cc31ad70cf8e112ec61889d3991ae
mahdi ahadpour <r4v3nk1n6@gmail.com>
2021-09-10 04:38:13 +0430

    adding some models to product - 2.5

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from customers.models import Customer
+from customers.models import Customer, Address
 from product.models import Product
 
 
@@ -9,10 +9,13 @@ class OrderDetails(models.Model):
     product = models.ForeignKey(Product, on_delete=models.RESTRICT)
     quantity = models.PositiveIntegerField()
     total = models.PositiveIntegerField()
-    fulfilled = models.BooleanField(default=False)
-    fulfilled_time = models.DateField()
-    order_date = models.DateTimeField()
+    fulfilled = models.BooleanField(default=False, editable=False)
+    fulfilled_time = models.DateField(editable=False)
+    order_date = models.DateTimeField(editable=False)
     costumer_massage = models.TextField()
+    address = models.ForeignKey(Address, on_delete=models.PROTECT)
+    shipment_method = models.CharField(max_length=100)
+    shipment_price = models.PositiveIntegerField()
 
 
 class Payment(models.Model):
@@ -28,3 +31,10 @@ class Orders(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.RESTRICT)
     order_detail = models.OneToOneField(OrderDetails, on_delete=models.PROTECT)
     deleted = models.BooleanField(default=False)
+
+
+class Shipment(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    shipment_statues = models.CharField(max_length=100)
+    shipper = models.CharField(max_length=100)
+    shipper_phone = models.CharField(max_length=11)
