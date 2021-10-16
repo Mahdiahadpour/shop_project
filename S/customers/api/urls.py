bf7ae2d87bc49f88999d08c74b3c03be7ac0d5d4
mahdi ahadpour <88606423+Mahdiahadpour@users.noreply.github.com>
2021-10-16 19:59:50 +0330

    Delete S directory

--- S/customers/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CustomerSignUp, AddressInput
-
-urlpatterns = [
-    path('signup', CustomerSignUp.as_view(), name='signUp'),
-    path('address', AddressInput.as_view(), name=' adressinput')
-]
--- S/orders/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import basket_view, delete_item_from_basket, checkout
-urlpatterns = [
-    path('basket/', basket_view, name='basket'),
-    path('basket/delete/', delete_item_from_basket, name='delete_item_from_basket'),
-    path('checkout/', checkout, name='checkout'),
-]
