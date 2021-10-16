afe8e8796ad84406e504bbc9c1af8e2876ea0de9
mahdi ahadpour <88606423+Mahdiahadpour@users.noreply.github.com>
2021-10-16 20:08:07 +0330

    update setting.py

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -76,14 +76,24 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'shop_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shop_db',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "WebServer_db",
+        "USER": "django_db",
+        "PASSWROD": "testdb",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
